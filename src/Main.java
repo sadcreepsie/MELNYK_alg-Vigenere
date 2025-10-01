650f3cdff5baf0f56bc7f062ca79631450ba18ff
Melnyk Roman <126952179@itisrossi.vi.it>
2025-10-01 09:11:48 +0200

    es finito

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,53 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-
+        Map<Character, Integer> alphabetMap = new HashMap<>();
+        for (char c = 'A'; c <= 'Z'; c++) {
+            alphabetMap.put(c, c - 'A');
         }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Insert text to cryptate: ");
+        String text = sc.nextLine();
+        System.out.print("Insert code: ");
+        String code = sc.nextLine();
+        List<Character> textOr = new ArrayList<>();
+        for (int i = 0; i < text.length(); i++) {
+            char upperChar = Character.toUpperCase(text.charAt(i));
+            textOr.add(upperChar);
+        }
+        List<Character> codeList = new ArrayList<>();
+        for (int i = 0; i < code.length(); i++) {
+            char upperChar = Character.toUpperCase(code.charAt(i));
+            codeList.add(upperChar);
+        }
+        String textCrypted = "";
+        Character charCrypted;
+        int cnt = 0;
+        for (int i = 0; i < text.length(); i++) {
+            Character symOr = textOr.get(i);
+            Character symCode;
+            if (i > codeList.size() - 1) {
+                if (cnt > codeList.size()-1){
+                    cnt = 0;
+                    symCode = codeList.get(cnt);
+                }
+                else {
+                    symCode = codeList.get(cnt);
+                }
+                cnt++;
+            }
+            else {
+                symCode = codeList.get(i);
+            }
+            int codeCrypt = alphabetMap.get(symCode) + alphabetMap.get(symOr);
+            if (codeCrypt > 26){
+                codeCrypt -= 26;
+            }
+            charCrypted = (char)('A' + codeCrypt);
+            textCrypted += charCrypted;
+        }
+        System.out.println(textCrypted);
     }
 }
